fc9aa5984f7187c30d22a1488961b1c443d5c979
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-06-18 14:56:00 +0530

    remove not null constraint for upgrades

--- features/mtwilson-postgresql/src/main/resources/com/intel/mtwilson/database/postgresql/20190409152200_update_mw_user_login_certificate.sql
+++ features/mtwilson-postgresql/src/main/resources/com/intel/mtwilson/database/postgresql/20190409152200_update_mw_user_login_certificate.sql
@@ -2,6 +2,6 @@
  * Author:
  * Created: Apr 09, 2019
  */
-ALTER TABLE mw_user_login_certificate ADD COLUMN sha384_hash bytea NOT NULL;
+ALTER TABLE mw_user_login_certificate ADD COLUMN sha384_hash bytea;
 
 INSERT INTO changelog (ID, APPLIED_AT, DESCRIPTION) VALUES (20190409152200,NOW(),'Added a column for sha384 digest algorithm in mw_user_login_certificate table');
