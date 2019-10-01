5e00dcd506b8e28b478562cb152540c3377bc8a4
Praveenraj R <praveenraj.r@intel.com>
2019-10-01 16:08:35 +0530

    v1.6/tasks/certificates-signing-from-cmsca:
    Change-Id: Id39ce00b4abc629b28f9b25172f3dd3f7ed60475

--- features/mtwilson-setup/src/main/java/com/intel/mtwilson/setup/utils/CertificateUtils.java
+++ features/mtwilson-setup/src/main/java/com/intel/mtwilson/setup/utils/CertificateUtils.java
@@ -1,29 +1,21 @@
+/*
+ * Copyright (C) 2019 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 package com.intel.mtwilson.setup.utils;
 
 import com.intel.dcsg.cpg.io.pem.Pem;
-import org.bouncycastle.asn1.x509.BasicConstraints;
-import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
-import org.bouncycastle.asn1.x509.X509Extensions;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
-import org.bouncycastle.x509.extension.AuthorityKeyIdentifierStructure;
 
 import javax.security.auth.x500.X500Principal;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.security.*;
-import java.security.cert.Certificate;
-import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Calendar;
 
 public class CertificateUtils {
 
