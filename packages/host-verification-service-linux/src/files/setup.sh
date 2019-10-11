e6ce4b45797951966948177fe2cc612fc0f99631
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-10-11 01:00:14 -0700

    revert RHEL 8 changes

--- packages/host-verification-service-linux/src/files/setup.sh
+++ packages/host-verification-service-linux/src/files/setup.sh
@@ -295,7 +295,7 @@ fi
 # 5.2 Install prerequisites
 if [ "$(whoami)" == "root" ]; then
   if [ "$IS_RPM" != "true" ]; then
-    MTWILSON_YUM_PACKAGES="zip unzip openssl wget net-tools python3-policycoreutils"
+    MTWILSON_YUM_PACKAGES="zip unzip openssl wget net-tools policycoreutils-python"
   fi
   MTWILSON_APT_PACKAGES="zip unzip openssl policycoreutils "
   MTWILSON_YAST_PACKAGES="zip unzip openssl"
